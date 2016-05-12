efbe1bf378b9989f84e7d5538e3ad273ae1fd923
Robert <robiswelt@gmail.com>
2016-05-12 14:25:56 +0200

    Init commit

---
+++ CQRSStart/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CQRSStart
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Available commands: Get, Quit");
+
+			while (true)
+			{
+				string command = Console.ReadLine();
+
+				if (IsQuitCommand(command))
+				{
+					Console.WriteLine("Quit is called. Shutdown system ....");
+					break;
+				}
+
+				RunCommand(command);
+			}
+
+			Console.ReadKey();
+		}
+
+		private static void RunCommand(string command)
+		{
+			switch (command.Trim().ToLower())
+			{
+				case "get":
+					GetAllData();
+					return;
+				default:
+					return;		
+			}			
+		}
+
+		private static void GetAllData()
+		{
+			Console.WriteLine("Call --> Get all data.");
+
+		}
+
+		private static bool IsQuitCommand(string command)
+		{
+			return command.Equals("quit", StringComparison.OrdinalIgnoreCase);
+		}
+	}
+}
